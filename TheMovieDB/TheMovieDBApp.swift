874040f051611bd2730cd94da0b9edf5063d9b97
Adeel Miraj <adeelmiraj@gmail.com>
2024-02-21 09:33:12 +0500

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Admin on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
