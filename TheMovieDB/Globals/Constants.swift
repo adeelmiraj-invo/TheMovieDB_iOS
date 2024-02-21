18f75b454b1bb83ca5e7ddf0cc9e9893daa66613
Adeel Miraj <adeelmiraj@gmail.com>
2024-02-21 10:00:20 +0500

    Directory structure changes and constants file creation.

---
+++ TheMovieDB/Globals/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TheMovieDB
+//
+//  Created by Admin on 21/02/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let movieDBAPIKey: String = "aa19265f25eaee4f5d40630f0f568cf6"
+    static let movieDBReadAccessToken: String = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTE5MjY1ZjI1ZWFlZTRmNWQ0MDYzMGYwZjU2OGNmNiIsInN1YiI6IjY1ZDU3Zjg2NjA5NzUwMDE4NTIzMTcxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1l76sXf4MxzZWJtU_xW4-wDCN2wrjADsUtuz5_tT7r4"
+}
