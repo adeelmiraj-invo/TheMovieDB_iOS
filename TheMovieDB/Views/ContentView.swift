30dac053662527d53e46096c2280d459d6ca0186
Adeel Miraj <adeelmiraj@gmail.com>
2024-02-22 10:35:33 +0500

    Install pods

--- TheMovieDB/Views/ContentView.swift
+++ TheMovieDB/Views/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var data  = Array(1...20)
+    private let adaptiveColumn = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        ScrollView{
+            LazyVGrid(columns: adaptiveColumn, spacing: 20) {
+                ForEach(data, id: \.self) { item in
+                    Text(String(item))
+                        .frame(width: 150, height: 150, alignment: .center)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.title)
+                }
+            }
+            
+        } .padding()
     }
 }
 
